847aac5c68dab7c5eb2b336a0f6d75dfdcc95b24
itsregalo <itsregalo047@gmail.com>
2023-03-29 00:35:59 +0300

    removed unnecessary links

--- Connecting_Http_Provider_Infura/main.py
+++
@@ -1,8 +0,0 @@
-from web3 import Web3
-from decouple import config
-# using a remote node
-
-w3 = Web3(Web3.HTTPProvider(config('ethereum_infura_endpoint')))
-
-# Check if connected to Ethereum node
-print(w3.is_connected())
--- Ethereum_Test_Node/readme.md
+++
@@ -1,9 +0,0 @@
-# Connecting to Test Node for Ethereum
-
-## 1. Install Eth_Test_Node
-
-* This is a test node for Ethereum. It is a private network with 10 accounts.
-
-```bash
-pip install eth_tester
-```
