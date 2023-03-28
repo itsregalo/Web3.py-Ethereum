cb3e51b5a718488ff0f762d901abb966a0a2cf6c
itsregalo <itsregalo047@gmail.com>
2023-03-28 23:55:34 +0300

    checking the block number

--- 2.Connecting_Http_Provider_Infura/main.py
+++ 2.Connecting_Http_Provider_Infura/main.py
@@ -6,8 +6,3 @@ w3 = Web3(Web3.HTTPProvider(config('ethereum_infura_endpoint')))
 
 # Check if connected to Ethereum node
 print(w3.is_connected())
-
-# check the block number
-block_number = w3.eth.blockNumber
-
-print("Block number: ", block_number)
