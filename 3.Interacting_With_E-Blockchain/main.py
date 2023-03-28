e2ab21ab4ef2952e4ffc55b4d9d4593d7001fcb6
itsregalo <itsregalo047@gmail.com>
2023-03-28 23:55:22 +0300

    checking the block number

---
+++ 3.Interacting_With_E-Blockchain/main.py
@@ -0,0 +1,13 @@
+from web3 import Web3
+from decouple import config
+# using a remote node
+
+w3 = Web3(Web3.HTTPProvider(config('ethereum_infura_endpoint')))
+
+# Check if connected to Ethereum node
+print(w3.is_connected())
+
+# check the block number
+block_number = w3.eth.block_number
+
+print("Block number: ", block_number)
